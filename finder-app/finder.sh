2f6a7afe1a8988e3d7e0df50faf9262372ddb038
euq <hpavlakis@disroot.org>
2023-07-16 21:02:29 +0300

    assignment3 part2 initial commit

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,23 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
-if [ $# -lt 2 ]; then
-  exit 1
+if [ $# -ne 2 ]; then
+	echo "wrong number of input argumets. exiting.."
+	exit 1
 fi
 
-filesdir="$1"
-searchstr="$2"
-
-if [ ! -d "$filesdir" ]; then
-  exit 1
+if [ ! -d "$1" ]; then
+	echo ""$1" is not a directory. exiting.."
+	exit 1
 fi
 
-file_count=0
-line_count=0
-
-while IFS= read -r -d '' file; do
-  file_count=$((file_count + 1))
-  count=$(grep -c "$searchstr" "$file")
-  line_count=$((line_count + count))
-done < <(find "$filesdir" -type f -print0)
-
-echo "The number of files are $file_count and the number of matching lines are $line_count"
+echo	"the number of files are" \
+		"$(find "$1" -type f | wc -l)" \
+		"and the number of matching lines are" \
+		"$(grep -r "$2" "$1" | wc -l)"
--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -23,7 +23,12 @@ fi
 
 mkdir -p ${OUTDIR}
 
-cd "$OUTDIR"
+if [ ! -d ${OUTDIR} ]; then
+    echo "creating $OUTDIR failed, exit.."
+    exit 1
+fi
+
+cd ${OUTDIR}
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
     #Clone only if the repository does not exist.
 	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
@@ -35,9 +40,16 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+	ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} make mrproper
+	ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} make defconfig
+	ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} make -j2
+	ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} make modules
+	ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} make dtbs
+
 fi
 
 echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -48,6 +60,10 @@ then
 fi
 
 # TODO: Create necessary base directories
+mkdir -p rootfs/bin rootfs/dev rootfs/etc rootfs/home rootfs/lib rootfs/lib64 rootfs/proc rootfs/sbin rootfs/sys rootfs/tmp rootfs/usr rootfs/var
+mkdir -p rootfs/usr/bin rootfs/usr/lib rootfs/usr/sbin
+mkdir -p rootfs/var/log
+
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -56,25 +72,61 @@ git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    make distclean
+    make defconfig
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+ROOTFS="${OUTDIR}/rootfs"
+make CONFIG_PREFIX=${ROOTFS} ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+
+cd ${ROOTFS}
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+SYSROOT=`${CROSS_COMPILE}gcc --print-sysroot`
+
+cp ${SYSROOT}/lib/ld-linux-aarch64.so.1 lib/
+cp ${SYSROOT}/lib64/libm.so.6 lib64/
+cp ${SYSROOT}/lib64/libresolv.so.2 lib64/
+cp ${SYSROOT}/lib64/libc.so.6 lib64/
 
 # TODO: Make device nodes
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 600 dev/console c 5 1
+
 
 # TODO: Clean and build the writer utility
+cd ${FINDER_APP_DIR}
+make clean
+make CROSS_COMPILE=$CROSS_COMPILE writer
+
+cp ${FINDER_APP_DIR}/writer ${ROOTFS}/home
+
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp ${FINDER_APP_DIR}/autorun-qemu.sh ${ROOTFS}/home/
+
+cp ${FINDER_APP_DIR}/finder.sh ${ROOTFS}/home/
+cp ${FINDER_APP_DIR}/finder-test.sh ${ROOTFS}/home/
+
+mkdir $ROOTFS/home/conf
+cp ${FINDER_APP_DIR}/conf/assignment.txt ${ROOTFS}/home/conf/
+cp ${FINDER_APP_DIR}/conf/username.txt ${ROOTFS}/home/conf/
+
 
 # TODO: Chown the root directory
+sudo chown -Rf root:root ${ROOTFS}
 
 # TODO: Create initramfs.cpio.gz
+cd ${ROOTFS}
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+cd ${OUTDIR}
+gzip -f initramfs.cpio
